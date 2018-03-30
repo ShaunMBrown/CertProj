76610c0a97746e95376b850cace722d155f7ce82
Shaun Brown <ShaunMBrown@users.noreply.github.com>
2018-03-30 01:14:55 -0700

    Add files via upload

---
+++ src/certproj/CertProj.java
@@ -0,0 +1,25 @@
+package certproj;
+
+import java.security.KeyStore;
+import java.security.Key;
+import java.security.PublicKey;
+import java.security.Signature;
+//import java.security.Cipher;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+
+public class CertProj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public static void openCert(String certName) {
+        
+    }
+    
+}
